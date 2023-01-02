41f159b9b69e683e151a55919cf16209209ca879
Misa <milosursulovic2696@gmail.com>
2023-01-02 20:42:52 +0100

    Add weather forecast list

--- app/src/main/java/com/example/weather/presentation/WeatherCard.kt
+++ app/src/main/java/com/example/weather/presentation/WeatherCard.kt
@@ -1,6 +1,5 @@
 package com.example.weather.presentation
 
-import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -11,7 +10,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.TextStyle
